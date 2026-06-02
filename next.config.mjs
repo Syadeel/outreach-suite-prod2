710e6664db06a864a657ed8f5972a74cb1bdaf91
Syadeel <adelshah020@gmail.com>
2026-06-02 05:21:44 +0500

    Config: allow remote images from microlink, cloudinary in next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.microlink.io' },
+      { protocol: 'https', hostname: '**.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+    ],
+  },
 };
 
 export default nextConfig;
